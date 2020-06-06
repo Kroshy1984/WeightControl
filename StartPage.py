d636efdc90ae9454b8ae08d6bf7317ccda23968d
Ваше Имя <raznomazov_vm@mail.ru>
2020-06-06 17:54:38 +0200

    Tkinter interface was changed.SQLPower was created

--- StartPage.py
+++ StartPage.py
@@ -16,19 +16,35 @@ class StartPage():
         self.Start.mainloop()
 
     def ToBase(self):
-        self.Comment = tkinter.Toplevel(self.Start)
-        self.Comment.geometry('300x200+200+200')
-        self.Comment.title("Желаете ли внести комментарий?")
-        label = tkinter.Label(self.Comment, text="Введите комментарий:", bg="lightgrey", fg="red")
-        label.place(x=10, y=10)
-        self.message_entry1 = tkinter.Entry(self.Comment, textvariable='')
-        self.message_entry1.place(x=10, y=50,width=260,height=50)
-        btn1 = tkinter.Button(self.Comment, text="Занести вес и комментарий в базу", bg="lightblue", fg="black",
+        self.b = self.message_entry.get()
+        print(self.b.find(","))
+        if self.b.find(",")>1:self.b=self.b.replace(",",".")
+        print(self.b)
+        try:
+         self.b1=float(self.b)
+        except ValueError:
+            print("Ошибка значения")
+            self.Comment = tkinter.Toplevel(self.Start)
+            self.Comment.geometry('300x200+200+200')
+            self.Comment.title("Ошибка значения")
+            label = tkinter.Label(self.Comment, text="Введите Заново!", bg="lightgrey", fg="red")
+            label.place(x=10, y=10)
+        else:
+            self.Comment = tkinter.Toplevel(self.Start)
+            self.Comment.geometry('300x200+200+200')
+            self.Comment.title("Желаете ли внести комментарий?")
+            label = tkinter.Label(self.Comment, text="Введите комментарий:", bg="lightgrey", fg="red")
+            label.place(x=10, y=10)
+            self.message_entry1 = tkinter.Entry(self.Comment, textvariable='')
+            self.message_entry1.place(x=10, y=50,width=260,height=50)
+            btn1 = tkinter.Button(self.Comment, text="Занести вес и комментарий в базу", bg="lightblue", fg="black",
                               command=self.SQLPower)
-        btn1.place(x=10, y=120)
+            btn1.place(x=10, y=120)
+            btn2 = tkinter.Button(self.Comment, text="Передумал заносить", bg="red", fg="black",
+                              command=lambda: self.Comment.destroy())
+            btn2.place(x=10, y=150)
 
     def SQLPower(self):
-        self.b= self.message_entry.get()
         self.a= self.message_entry1.get()
         self.f = str(datetime.datetime.now())
         print(self.f)
